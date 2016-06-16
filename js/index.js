2c851fe604b5a20132096fcc91ab88e0e25a376d
susu <28025920@qq.com>
2016-06-16 12:37:53 +0800

    添加Js动态响应

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 /**
  * Created by jiangsu on 16/6/16.
  */
+
+function $(id){
+    return typeof id==='string' ? document.getElementById(id):id;
+}
+
+window.onload=function(){
+
+    //拿到所有的Li标签和Li对应内容的div
+    var  titles=$('tab-header').getElementsByTagName('li');
+    var  divs=$('tab-content').getElementsByTagName('div');
+
+    //判断
+    if(titles.length!=divs.length) return;
+
+    for(var i=0;i<titles.length;i++){
+        var  li=titles[i];
+        li.id=i;
+
+        li.onmousemove=function() {
+            for (var j = 0; j < titles.length; j++) {
+                titles[j].className = '';
+                divs[j].style.display = 'none';
+            }
+            //让当前被选中
+            this.className = 'select';
+            divs[this.id].style.display = 'block';
+        }
+    }
+}
